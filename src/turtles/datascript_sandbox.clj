e9266d6b6c720800f7bff6f935891a32beb5a432
Calvin Sauer <calvin.j.sauer@gmail.com>
2018-08-08 15:59:27 -0400

    Playing with ds

--- src/turtles/datascript_sandbox.clj
+++ src/turtles/datascript_sandbox.clj
@@ -1,40 +1,120 @@
 (ns turtles.datascript-sandbox
-  (:require [datascript.core :as d]))
+  (:require [datascript.core :as d]
+            [quil.core :as q]
+            [turtles.math :as math]))
 
-(def schema
-  {:eid {:db/unique :db.unique/identity}})
+(def world-size 1000)
 
-(defn new-eid
+(defn now
   []
-  (java.util.UUID/randomUUID))
+  (System/currentTimeMillis))
 
-(defn create-turtle
-  [attrs]
-  (let [db (d/create-conn schema)
-        eid (new-eid)
-        attrs* (assoc attrs :eid eid)]
-    (alter-meta! db #(assoc % :self [:eid eid]))
-    (d/transact! db [attrs*])
-    db))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Database
 
-(defn self
-  [turtle]
-  (d/pull @turtle '[*] (:self (meta turtle))))
-
-(def turtle1 (create-turtle {:coord [0 0]}))
-(def turtle2 (create-turtle {:coord [0 10]}))
-(def turtle3 (create-turtle {:coord [-5 12]}))
-
-(defn current-time-seconds
+(defn rand-color
   []
-  (/ (System/currentTimeMillis) 1000))
+  (into [] (repeatedly 3 #(rand-int 256))))
 
-(def fact1 {:origin [5 5]
-            :speed 0.5
-            :t (current-time-seconds)
-            :fact {:pheremone 1}})
+(defn rand-pos
+  [x y]
+  [(rand-int x) (rand-int y)])
 
-(defn fact-snapshot
-  [{:keys [t speed] :as fact}]
-  (let [dt (- (current-time-seconds) t)]
-    (assoc fact :radius (* dt speed))))
+(defn transact!
+  "Same as d/transact! except also decorates the transaction with
+  :db/txInstant metadata, whose value is the current time in milliseconds."
+  [conn tx-data]
+  (let [current-time (System/currentTimeMillis)
+        tx-instant [[:db/add :db/current-tx :db/txInstant current-time]]]
+   (d/transact! conn (concat tx-data tx-instant))))
+
+(defn- create-turtle
+  []
+  {:color (rand-color)
+   :position (rand-pos world-size world-size)
+   :velocity [(dec (rand 2)) (dec (rand 2))]})
+
+(defn create-turtles!
+  [conn n]
+  (transact! conn (for [_ (range n)]
+                    (create-turtle)))
+  conn)
+
+(defn create-universe
+  []
+  (d/create-conn))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Daemons
+
+(defprotocol IAutonomous
+  (observe [self db] "Returns some filtered view of db from the perspective of self.")
+  (process [self observation] "Returns transaction data based on the observation results."))
+
+(defn- velocity-tx
+  [db db-id dt]
+  (let [self (d/entity db db-id)
+        new-pos (math/coord+ (:position self)
+                             (math/coord* (:velocity self) dt))]
+    {:db/id db-id
+     :position new-pos}))
+
+(def velocity-ms (atom (System/currentTimeMillis)))
+(defn velocity-daemon
+  [{:keys [conn] :as ctx}]
+  (send *agent* velocity-daemon)
+  (let [dt (- (now) @velocity-ms)
+        db @conn
+        es (d/q '[:find [?e ...]
+                  :where
+                  [?e :position]
+                  [?e :velocity]]
+                db)
+        txs (map #(velocity-tx db % 1.0) es)]
+    (d/transact! conn txs)
+    (reset! velocity-ms (now))
+    ctx))
+
+(defn start-daemon
+  [conn daemon-fn]
+  (let [daemon (agent {:conn conn})]
+    (send daemon daemon-fn)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; UI
+
+(defn render-universe
+  [db]
+  (let [renderables (d/q '[:find ?color ?pos
+                           :where
+                           [?e :color ?color]
+                           [?e :position ?pos]]
+                         db)]
+    (q/fill 0 0 0)
+    (q/rect 0 0 world-size world-size)
+    (doseq [[[r g b] [x y]] renderables]
+      (q/fill r g b)
+      (q/ellipse x y 10 10))))
+
+(defn- setup-sketch
+  []
+  (q/background 0)
+  (q/frame-rate 30))
+
+(defn start-sketch
+  [conn]
+  (q/sketch
+   :title "Turtles"
+   :setup setup-sketch
+   :draw #(render-universe @conn)
+   :size [world-size world-size]))
+
+(comment
+
+  (do
+    (def conn (create-universe))
+    (def ui (start-sketch conn))
+    (create-turtles! conn 500)
+    true)
+
+  )
